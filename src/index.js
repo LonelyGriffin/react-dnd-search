683d5ccce0b548ca65af3ea042e3724aa115c0d5
Fanaskov Pavel <lonelygriffin@gmail.com>
2017-07-09 12:17:38 +0300

    step 1 | add DragDropContextProvider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {DragDropContextProvider} from "react-dnd";
+import HTML5Backend from 'react-dnd-html5-backend';
 
 ReactDOM.render(
-    <App />
+    <DragDropContextProvider backend={HTML5Backend}>
+        <App />
+    </DragDropContextProvider>
 , document.getElementById('root'));
