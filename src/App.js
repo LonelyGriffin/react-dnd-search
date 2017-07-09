0847c078c275a944fc027bb355cb656b77e759a0
Fanaskov Pavel <lonelygriffin@gmail.com>
2017-07-09 12:55:51 +0300

    step 3 | add Square

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Knight} from "./Knight";
 import "./App.css";
+import {Square} from "./Square";
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Knight/>
+        <Square/>
+        <Square isWhite={true}/>
       </div>
     );
   }
---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Square extends React.Component {
+    render() {
+        const {isWhite} = this.props;
+        const colorModifier = isWhite ? "square_white" : "square_black";
+        return (
+            <div className={`square ${colorModifier}`}></div>
+        );
+    }
+}
