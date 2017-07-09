893a2b2c8fa423bb08bacfa081b0a175631df4a5
Fanaskov Pavel <lonelygriffin@gmail.com>
2017-07-09 16:25:36 +0300

    step 8 | add square drop

--- src/Board.js
+++ src/Board.js
@@ -3,13 +3,39 @@ import {Square} from "./Square";
 import {Knight} from "./Knight";
 
 export class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            knightPositionX: props.knightPositionX,
+            knightPositionY: props.knightPositionY,
+        }
+    }
+
+    moveKnight = (x, y) => {
+        if(x >= 0 && x < 8 && y >= 0 && y < 8) {
+            this.setState({
+                knightPositionX: x,
+                knightPositionY: y,
+            });
+        }
+    };
+
+    onDropKnight = (x, y) => {
+        this.moveKnight(x, y);
+    };
+
     renderRow(rowNumber, knightPositionX, knightPositionY) {
         const range8 = Array.from(new Array(8).keys());
         return (
             <div className="board__row" key={rowNumber}>
                 {
                     range8.map(colNumber =>
-                        <Square isWhite={(rowNumber + colNumber) % 2 === 0} key={colNumber}>
+                        <Square
+                            isWhite={(rowNumber + colNumber) % 2 === 0} key={colNumber}
+                            x={rowNumber}
+                            y={colNumber}
+                            onDropKnight={this.onDropKnight}
+                        >
                             {knightPositionX === rowNumber && knightPositionY === colNumber && <Knight/>}
                         </Square>
                     )
@@ -19,7 +45,7 @@ export class Board extends React.Component {
     }
 
     render() {
-        const {knightPositionX, knightPositionY} = this.props;
+        const {knightPositionX, knightPositionY} = this.state;
         const range8 = Array.from(new Array(8).keys());
         return (
             <div className="board">
--- src/Square.js
+++ src/Square.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { DropTarget } from 'react-dnd';
+import * as DnDItems from "./dndItems";
 
-export class Square extends React.Component {
+const squareTarget = {
+    drop(props, monitor, component) {
+        props.onDropKnight(props.x, props.y);
+    },
+};
+
+const squareCollect = (connect, monitor) => {
+    return {
+        connectDropTarget: connect.dropTarget(),
+    };
+};
+
+export class SquareComponent extends React.Component {
     render() {
-        const {isWhite} = this.props;
+        const {isWhite, connectDropTarget} = this.props;
         const colorModifier = isWhite ? "square_white" : "square_black";
-        return (
+        return (connectDropTarget(
             <div className={`square ${colorModifier}`}>
                 {this.props.children && React.cloneElement(this.props.children, { isInWhiteSquare: isWhite })}
             </div>
-        );
+        ));
     }
 }
+
+export const Square = DropTarget(DnDItems.KNIGHT, squareTarget, squareCollect)(SquareComponent);
